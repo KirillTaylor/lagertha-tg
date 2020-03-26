ef9150cb4a2c16c6cc058166a067c0da468fe83c
Kirill Shvetsov <kirill86@gmail.com>
2020-03-26 10:36:06 +0200

    Added an in-memory DB

--- lagertha.js
+++ lagertha.js
@@ -3,6 +3,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const db = require('./db.json');
 
 
 function between(min, max) {  
@@ -12,7 +13,7 @@ function between(min, max) {
 }
 
 client.on('ready', () => {
-	const game = 'Ragnarok';
+	const game = config.game;
 	console.log(`Logged in as ${client.user.tag}!`);
 	client.user.setStatus('available');
 	client.user.setPresence({
@@ -21,19 +22,18 @@ client.on('ready', () => {
             type: 0
         }
     });
-	/* client.channels.map((channel) => {
-		if(channel.type === 'text'){
-			channel.send(`LargerthaBot reloaded. (Version ${config.version})`);
-			if(game){
-				channel.send("I'm now playing " + game);
-			}
-			channel.send("Hit me up with !bobs or !ping");
-		}
-	}); */
+	// client.channels.map((channel) => {
+	// 	if(channel.type === 'text'){
+	// 		channel.send(`LargerthaBot reloaded. (Version ${config.version})`);
+	// 		if(game){
+	// 			channel.send("I'm now playing " + game);
+	// 		}
+	// 		channel.send("sup");
+	// 	}
+	// });
 });
 
 client.on('message', msg => {
-	console.log(msg);
 	const parsedCMD = msg.content.substr(1);
 	if (parsedCMD === 'ping') {
 		msg.reply('Pong!');
@@ -55,27 +55,35 @@ client.login(config.discord.token);
 const TGBot = require('node-telegram-bot-api');
 
 const bot = new TGBot(config.telegram.token, {polling: true});
+let messages = db.messages;
+
+function randomMessage(){    
+    return messages[between(0, messages.length - 1)];
+}
+
+bot.sendMessage(-1001393546769, `[Lagertha] Updated to version ${config.version}. \nRandomness level: ${config.randomness * 100}%`);
 
 bot.on('message', (msg) => {
     console.log(msg);
     if(msg.text){
         const parsedCMD = msg.text.substr(1);
-    
+        messages.push(msg.text);
         if (parsedCMD === 'ping') {
             bot.sendMessage(msg.chat.id, 'Pong!');
-        }
-        if (msg.text === 'smaw'){
+        } else if (msg.text === 'smaw'){
             bot.sendMessage(msg.chat.id, "smiiiiii");
-        }
-        
-        if (parsedCMD === 'bobs'){
+        } else if (parsedCMD === 'bobs'){
             const randomNumber = between(1, 18);
             bot.sendPhoto(msg.chat.id, `./bobs/${randomNumber}.jpg`);
-        }
-
-        if(msg.text.includes('azov')){
+        } else if(msg.text.includes('azov')){
             bot.sendMessage(msg.chat.id, `@${msg.from.username} halaasssss`);
+        } else {
+            console.log(parseFloat(config.randomness));
+            if(Math.random() <= parseFloat(config.randomness)){
+                bot.sendMessage(msg.chat.id, randomMessage());
+            }
         }
+        console.log(messages);
     }
 });
 
