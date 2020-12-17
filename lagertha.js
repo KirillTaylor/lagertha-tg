9246cd2b52b32bdce425e6023d6c917eb4f4a9e3
Kirill Shvetsov <kirill.s@next-insurance.com>
2020-12-17 21:09:02 +0200

    removed Discord API

--- lagertha.js
+++ lagertha.js
@@ -1,54 +1,12 @@
-/** Lagertha for Discord bot **/
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const config = require('./config.json');
 const db = require('./db.json');
-
 
 function between(min, max) {  
   return Math.floor(
     Math.random() * (max - min) + min
   )
 }
-
-client.on('ready', () => {
-	const game = config.game;
-	console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setStatus('available');
-	client.user.setPresence({
-        game: {
-            name: game,
-            type: 0
-        }
-    });
-	// client.channels.map((channel) => {
-	// 	if(channel.type === 'text'){
-	// 		channel.send(`LargerthaBot reloaded. (Version ${config.version})`);
-	// 		if(game){
-	// 			channel.send("I'm now playing " + game);
-	// 		}
-	// 		channel.send("sup");
-	// 	}
-	// });
-});
-
-client.on('message', msg => {
-	const parsedCMD = msg.content.substr(1);
-	if (parsedCMD === 'ping') {
-		msg.reply('Pong!');
-	}
-	if (msg.content === 'smaw'){
-		msg.channel.send("smiiiiii");
-	}
-	
-	if (parsedCMD === 'bobs'){
-		const randomNumber = between(1, 18);
-		msg.reply("There you go", {files: [`./bobs/${randomNumber}.jpg`]});
-	}
-});
-
-client.login(config.discord.token);
 
 // Lagertha for Telegram **/
 
